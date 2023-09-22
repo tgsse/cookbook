0ca94940792f0aa356af192137caff81a18f8d5b
tgsse <tamas.gura.sse@gmail.com>
2023-09-22 20:07:21 +0200

    added colors

---
+++ app/src/main/java/com/ix/cookbook/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.ix.cookbook.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val purple = Color(0xFF6200EA)
+val deepPurple = Color(0xFF5502C8)
+val lightPurple = Color(0xFF9D46FF)
+
+val lightGray = Color(0xFFF9F9F9)
+val mediumGray = Color(0xFFD1D1D1)
+val lightMediumGray = Color(0x80D1D1D1)
+
+val dark = Color(0xFF313C44)
+val darker = Color(0xFF151B20)
+
+val red = Color(0xFFFF4646)
+val yellow = Color(0xFFFFC114)
+val green = Color(0xFF00C980)
