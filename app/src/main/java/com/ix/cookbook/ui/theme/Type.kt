8b6cc5a661960a87442073bf7608c66d2ddfdecd
tgsse <tamas.gura.sse@gmail.com>
2023-09-25 16:08:24 +0200

    added fonts

--- app/src/main/java/com/ix/cookbook/ui/theme/Type.kt
+++ app/src/main/java/com/ix/cookbook/ui/theme/Type.kt
@@ -2,18 +2,53 @@ package com.ix.cookbook.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ix.cookbook.R
+
+/*
+M3	Default Font Size/Line Height
+
+displayLarge	Roboto              57/64
+displayMedium	Roboto              45/52
+displaySmall	Roboto              36/44
+headlineLarge	Roboto              32/40
+headlineMedium	Roboto              28/36
+headlineSmall	Roboto              24/32
+titleLarge	    New- Roboto Medium  22/28
+titleMedium	    Roboto Medium       16/24
+titleSmall	    Roboto Medium       14/20
+bodyLarge	    Roboto              16/24
+bodyMedium	    Roboto              14/20
+bodySmall	    Roboto              12/16
+labelLarge	    Roboto Medium       14/20
+labelMedium	    Roboto Medium       12/16
+labelSmall	    New Roboto Medium,  11/16
+* */
+
+object Fonts {
+    val title = FontFamily(Font(R.font.indieflower_regular))
+    val body = FontFamily(Font(R.font.notosans_regular))
+}
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    bodyMedium = TextStyle(
+        fontFamily = Fonts.body,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Fonts.body,
+        fontSize = 14.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Fonts.title,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+//        letterSpacing = 0.15.sp,
     ),
     /* Other default text styles to override
     titleLarge = TextStyle(
